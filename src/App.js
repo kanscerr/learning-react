e4c126b480c79f61e4c4af6934e626b56237fa82
kanscerr <92222438+kanscerr@users.noreply.github.com>
2023-01-18 21:06:35 +0530

    github prject using useState hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import Colorize from './colorize';
-import UseEffect from './useEffect';
+// import UseEffect from './useEffect';
+import GetGithubUser from './github-user';
 import './App.css';
 
 function App() {
   return(
     <div className="App">
       {/* <Colorize/> */}
-      <UseEffect/>
+      {/* <UseEffect/> */}
+      <GetGithubUser/>
     </div>
   )
 }
---
+++ src/github-user.js
@@ -0,0 +1,73 @@
+//https://api.github.com/users/username
+import { useState } from 'react';
+
+const GetGithubUser = () =>{
+    let [display, setDisplay] = useState('none');
+    const [detail, setDetail] = useState('');
+    let [error, setError] = useState('');
+    const showDetail = (event) => {
+        try{
+            fetch(`https://api.github.com/users/${event.target.value}`)
+            .then(data => data.json())
+            .then((res) => {
+                
+                if(res.message){
+                    display = 'none';
+                    setDisplay(display);
+                    setError(res.message);
+                }
+                else{
+                    display = 'block';
+                    setError(null);
+                    setDisplay(display);
+                    console.log(res);
+                    setDetail(res);
+                }
+            });
+        }catch(error){
+            throw new Error('error');
+        }
+    }
+    return(
+        <div className="githubUser">
+            <h1>User Details</h1>
+            <input onChange= { (event) => showDetail(event) } placeholder="search user"></input>
+            <p>{ error }</p>
+            <div className="userInfo" style={{display: display}}>
+                <img src= { detail.avatar_url } alt="user"></img>
+                <h3>Username- { detail.login }</h3>
+                <h3>Name- { detail.name }</h3>
+                <h3>Bio- { detail.bio }</h3>
+                <h3>Repos- { detail.public_repos }</h3>
+                <h3>Following- { detail.following }</h3>
+                <h3>Followers- { detail.followers }</h3>
+            </div>
+        </div>
+    )
+}
+
+export default GetGithubUser;
+
+
+
+// const[details, showDetails] = useState();
+//     const fetchUser = (e) =>{
+//         fetch(`https://api.github.com/users/${e}`)
+//         .then(data => data.json())
+//             .then((res) => {
+//                 if(res.message){
+//                     console.log(res.message);
+//                 }
+//                 else{
+//                     console.log(res);
+//                     showDetails(res.login)
+//                     // showDetails(res.avatar_url);
+//                 }
+//             })
+//             .catch((error) =>{
+//                 console.log(error.message);
+//             })
+//     }
+//     const checkUser = (e) =>{
+//         fetchUser(e);
+//     }
